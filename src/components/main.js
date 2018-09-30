4f3cb90fd52aa7a1d6a47f48a7ae21d9be8393ab
zarab <musfiqzarab@iut-dhaka.edu>
2018-10-01 00:52:57 +0600

    Skeleton

---
+++ src/components/main.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Landing from "./landingPage";
+import About from "./aboutMe";
+import Contact from "./contact";
+import Projects from "./projects";
+import Resume from "./resume";
+import { Swtich, Route } from "react-router-dom";
+
+class Main extends Component {
+  state = {};
+  render() {
+    return (
+      <switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/aboutMe" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/resume" component={Resume} />
+      </switch>
+    );
+  }
+}
+
+export default Main;
